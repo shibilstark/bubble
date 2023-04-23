02fce5bc02eef7124b447ec5d4d0d784ca480d62
Shibil Hassan <97911178+shibilstark@users.noreply.github.com>
2023-04-24 01:39:37 +0530

    removed endpoint

--- lib/config/constants/appwrite_constants.dart
+++ lib/config/constants/appwrite_constants.dart
@@ -1,5 +1,4 @@
 class AppWriteConstants {
   static const projectId = "644117f39613a0f807d5";
   static const databaseId = "644118270ec3d1e62857";
-  static const endpoint = "http://192.168.1.6:7070/v1";
 }
