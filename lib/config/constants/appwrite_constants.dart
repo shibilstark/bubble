1c6efa1bea781f696bcbe499c0bb8842aef90afc
shibilstark <shibilhassankmangode@gmail.com>
2023-04-24 14:05:09 +0530

    Improvement in if conditions

--- lib/config/constants/appwrite_constants.dart
+++ lib/config/constants/appwrite_constants.dart
@@ -1,4 +1,6 @@
 class AppWriteConstants {
+  static const ipAddress = "YOUR IP ADDRESS";
   static const projectId = "644117f39613a0f807d5";
   static const databaseId = "644118270ec3d1e62857";
+  static const endpoint = "http://$ipAddress/v1";
 }
