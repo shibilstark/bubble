98f520ea56c2031bcec6a1dea074dce6a42a717f
shibilstark <shibilhassankmangode@gmail.com>
2023-05-14 20:30:29 +0530

    Implemented Auth Screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
-import 'package:bubble/config/themes/palette.dart';
+import 'package:bubble/config/themes/custom_themes.dart';
 import 'package:bubble/core/injections/injection_setup.dart';
 import 'package:bubble/domain/app_db/app_db_repository.dart';
+import 'package:bubble/presentation/bloc/theme/theme_bloc.dart';
 import 'package:bubble/presentation/router/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   await initializeAppDependancies();
-  runApp(const MyApp());
+  runApp(const Bubble());
 }
 
 Future<void> initializeAppDependancies() async {
@@ -16,30 +18,35 @@ Future<void> initializeAppDependancies() async {
   await getIt<AppDbRepository>().initializeDB();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Bubble extends StatelessWidget {
+  const Bubble({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(360, 900),
-        splitScreenMode: true,
-        minTextAdapt: true,
-        builder: (context, child) {
-          return const MaterialApp(
-            // TODO ThemeState
-            // theme: ThemeData(
-            //   scaffoldBackgroundColor: Palette.white,
-            //   appBarTheme: const AppBarTheme(
-            //     elevation: 0,
-            //     backgroundColor: Palette.white,
-            //   ),
-            //   fontFamily: "WorkSans",
-            // ),
-            debugShowCheckedModeBanner: false,
-            initialRoute: AppRouter.SPLASH_SCREEN,
-            onGenerateRoute: AppRouter.onGeneratedRoute,
-          );
-        });
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<ThemeBloc>(),
+        ),
+      ],
+      child: ScreenUtilInit(
+          designSize: const Size(360, 900),
+          splitScreenMode: true,
+          minTextAdapt: true,
+          builder: (context, child) {
+            context.read<ThemeBloc>().add(const LoadTheme());
+            return BlocBuilder<ThemeBloc, ThemeState>(
+              builder: (context, state) {
+                return MaterialApp(
+                  theme:
+                      state.isDarkMode ? CustomThemes.dark : CustomThemes.light,
+                  debugShowCheckedModeBanner: false,
+                  initialRoute: AppRouter.AUTH_SCREEN,
+                  onGenerateRoute: AppRouter.onGeneratedRoute,
+                );
+              },
+            );
+          }),
+    );
   }
 }
---
+++ lib/presentation/screens/auth/auth_screen.dart
@@ -0,0 +1,144 @@
+import 'package:bubble/config/assets/assets.dart';
+import 'package:bubble/config/themes/themes.dart';
+import 'package:bubble/presentation/widgets/asset_image.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        children: [
+          const AuthBackgroundWidget(),
+          Padding(
+            padding: AppPadding.largeScreenPadding,
+            child: Column(
+              children: const [
+                WelcomeTextsWidget(),
+                Spacer(),
+                AuthButtonWidget(),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
+
+class AuthButtonWidget extends StatelessWidget {
+  const AuthButtonWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: MaterialButton(
+            elevation: 3,
+            color: AppColors.white,
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+            onPressed: () {},
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const AppAssetImageView(
+                  SvgAsset.googleLogo,
+                  fit: BoxFit.scaleDown,
+                ),
+                WhiteSpace.gapW10,
+                Text(
+                  "Login or Signup",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(),
+                ),
+              ],
+            ),
+          ),
+        ),
+        WhiteSpace.gapH10,
+        Text.rich(
+          TextSpan(
+              text: "By creating account you are agreed to our ",
+              style: Theme.of(context).textTheme.bodySmall!,
+              children: [
+                TextSpan(
+                  text: "terms and conditions ",
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: AppColors.theme,
+                        fontWeight: AppFontWeight.medium,
+                      ),
+                ),
+                const TextSpan(
+                  text: "& ",
+                ),
+                TextSpan(
+                  text: "privacy policy  ",
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: AppColors.theme,
+                        fontWeight: AppFontWeight.medium,
+                      ),
+                ),
+              ]),
+        )
+      ],
+    );
+  }
+}
+
+class WelcomeTextsWidget extends StatelessWidget {
+  const WelcomeTextsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "BUBBLE",
+          style: TextStyle(
+            color: AppColors.theme,
+            fontSize: AppFontSize.titleLarge,
+            fontWeight: AppFontWeight.bold,
+          ),
+        ),
+        WhiteSpace.gapH20,
+        Text(
+          "Welcome to Bubble",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        WhiteSpace.gapH10,
+        Text(
+          "Expand your vision, connect with world ",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+}
+
+class AuthBackgroundWidget extends StatelessWidget {
+  const AuthBackgroundWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      alignment: Alignment.centerRight,
+      child: AppAssetImageView(
+        SvgAsset.appLogo,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
